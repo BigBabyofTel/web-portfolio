0ed05e56df778f2875e4803adba7517d2f2cb88f
BigBabyofTel <augustus.tb@outlook.com>
2024-03-19 06:26:27 +0200

    new background image added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 export default function Header() {
   return (
-    <header className="w-full h-[400px] border p-5 text-center flex flex-col bg-[url('/public/sinai.jpg')] bg-center">
-      <h1 className="text-5xl text-black dark:text-white lg:text-8xl">
+    <header className="w-full h-[400px] border text-center flex flex-col bg-[url('/public/sinai.jpg')] bg-cover bg-bottom bg-no-repeat justify-center">
+      <div className=" bg-gray-500 opacity-75">
+      <h1 className="text-3xl text-black dark:text-white lg:text-8xl">
         T. Augustus Baker
       </h1>
-      <h2 className="text-3xl text-black dark:text-white lg:text-7xl">
+      <h2 className="text-xl text-black dark:text-white lg:text-7xl">
         Aspiring Web Dev
       </h2>
+      </div>
     </header>
   );
 }
