63bdab44ae96d9dde288c8ac714c437de60d3b34
BigBabyofTel <augustus.tb@outlook.com>
2024-03-22 16:04:00 +0200

    nav adjusted

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ export default function Nav() {
   return (
     <div className="flex justify-between">
       <ModeToggle/>
-       <div className="text-center p-5 my-1 bg-[#9BCBD7] dark:bg-[#005a7d] rounded-3xl shadow-lg shadow-[#bf907e] dark:shadow-[#9bcbd7]">
+       <div className="text-center p-5 mt-1 mb-4 bg-[#9BCBD7] dark:bg-[#005a7d] rounded-3xl shadow-lg shadow-[#bf907e] dark:shadow-[#9bcbd7]">
           <a href="#aboutMe" className="p-2 text-[#005a7d] dark:text-[#bf907e]"><strong>About Me</strong></a>
           <a href="#skills" className="p-2 text-[#005a7d] dark:text-[#bf907e]"><strong>Skills</strong></a>
           <a href="#exhibits" className="p-2 text-[#005a7d] dark:text-[#bf907e]"><strong>Exhibits</strong></a>
